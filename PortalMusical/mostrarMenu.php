79183c7b7b64e73a21393001f1367f1c402fab57
JTM2606 <59911260+JTM2606@users.noreply.github.com>
2020-02-14 13:31:55 +0100

    Delete mostrarMenu.php

--- PortalMusical/mostrarMenu.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Has iniciado Sesion: ".$_SESSION['usuario'];
-		
-		echo "<p><a href='downmusic.php'>Descargar Musica</a></p>";
-		echo "<p><a href='.php'>Historial Facturas</a></p>";
-		
-		echo "<p><a href='pagina3.php'>Cerrar Sesion</a></p>";
-?>
